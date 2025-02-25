50836126ddf426c276800916d886a6aadc800072
wellington.pereira <wellington.pereira@transfero.com>
2025-02-25 14:48:01 -0300

    init project

---
+++ book-social/src/main/java/br/com/book/social/BookSocialApplication.java
@@ -0,0 +1,13 @@
+package br.com.book.social;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookSocialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookSocialApplication.class, args);
+	}
+
+}
---
+++ book-social/src/test/java/br/com/book/social/BookSocialApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.book.social;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookSocialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
