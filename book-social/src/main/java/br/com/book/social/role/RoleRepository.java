0cbd7da1ced4fc1ccbd4411e8e1294f577071b10
wellington.pereira <wellington.pereira@transfero.com>
2025-03-09 01:28:26 -0300

    set repository

---
+++ book-social/src/main/java/br/com/book/social/role/RoleRepository.java
@@ -0,0 +1,9 @@
+package br.com.book.social.role;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RoleRepository extends JpaRepository<Role, Integer> {
+    Optional<Role> findByName(String roleStudent);
+}
