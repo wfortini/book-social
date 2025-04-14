c77d80f1c91e63d1b2fd445bb845b26e02cfbf2e
wellington.pereira <wellington.pereira@transfero.com>
2025-04-14 09:04:04 -0300

    added entities

---
+++ book-social/src/main/java/br/com/book/social/book/Book.java
@@ -0,0 +1,25 @@
+package br.com.book.social.book;
+
+import br.com.book.social.common.BaseEntity;
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+@Getter
+@Setter
+@SuperBuilder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Book extends BaseEntity {
+    private String title;
+    private String authorName;
+    private String isbn;
+    private String synopsis;
+    private String bookCover;
+    private boolean archived;
+    private boolean shareable;
+}
---
+++ book-social/src/main/java/br/com/book/social/feedback/Feedback.java
@@ -0,0 +1,22 @@
+package br.com.book.social.feedback;
+
+import br.com.book.social.common.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+@Getter
+@Setter
+@SuperBuilder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Feedback extends BaseEntity {
+    @Column
+    private Double note;
+    private String comment;
+}
