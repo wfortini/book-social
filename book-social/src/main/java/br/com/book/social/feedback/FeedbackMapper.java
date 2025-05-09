e3b40f750ee86cbab59ece4d3e1873f9e30b9b62
wellington.pereira <wellington.pereira@transfero.com>
2025-05-08 22:13:33 -0300

    get all feedbacks

---
+++ book-social/src/main/java/br/com/book/social/feedback/FeedbackMapper.java
@@ -0,0 +1,29 @@
+package br.com.book.social.feedback;
+
+import br.com.book.social.book.Book;
+
+import java.util.Objects;
+
+public class FeedbackMapper {
+
+    public Feedback toFeedback(FeedbackRequest request) {
+        return Feedback.builder()
+                .note(request.note())
+                .comment(request.comment())
+                .book(Book.builder()
+                        .id(request.bookId())
+                        .shareable(false) // Not required and has no impact :: just to satisfy lombok
+                        .archived(false) // Not required and has no impact :: just to satisfy lombok
+                        .build()
+                )
+                .build();
+    }
+
+    public FeedbackResponse  toFeedbackResponse(Feedback feedback, Integer id) {
+        return FeedbackResponse.builder()
+                .note(feedback.getNote())
+                .comment(feedback.getComment())
+                .ownFeedback(Objects.equals(feedback.getCreatedBy(), id))
+                .build();
+    }
+}
---
+++ book-social/src/main/java/br/com/book/social/feedback/FeedbackResponse.java
@@ -0,0 +1,15 @@
+package br.com.book.social.feedback;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class FeedbackResponse {
+
+    private Double note;
+    private String comment;
+    private boolean ownFeedback;
+}
