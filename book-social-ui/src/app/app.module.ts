8c2d57cb674b98543bb72fdf33ae2b6143556ccf
wellington.pereira <wellington.pereira@transfero.com>
2025-06-24 22:06:03 -0300

    register

--- book-social-ui/src/app/app.module.ts
+++ book-social-ui/src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './modules/book/components/menu/menu/menu.component';
+import { LoginComponent } from './pages/login/login/login.component';
+import { ActivateAccountComponent } from './pages/activate-account/activate-account/activate-account.component';
+import { RegisterComponent } from './pages/register/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent
+    MenuComponent,
+    LoginComponent,
+    ActivateAccountComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
