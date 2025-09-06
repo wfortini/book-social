c823f2d28758f4f5e77b361549d8d25b34751b46
wellington.pereira <wellington.pereira@transfero.com>
2025-09-06 19:48:49 -0300

    added key cloak services

--- book-social/src/main/java/br/com/book/social/security/SecurityConfig.java
+++ book-social/src/main/java/br/com/book/social/security/SecurityConfig.java
@@ -44,9 +44,8 @@ public class SecurityConfig {
                                 .anyRequest()
                                 .authenticated()
                 )
-                .sessionManagement(session -> session.sessionCreationPolicy(STATELESS));
-                //.authenticationProvider(authenticationProvider);
-                //.addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+                .oauth2ResourceServer(auth ->
+                        auth.jwt(token -> token.jwtAuthenticationConverter(new KeycloakJwtAuthenticationConverter())));
 
         return http.build();
     }
