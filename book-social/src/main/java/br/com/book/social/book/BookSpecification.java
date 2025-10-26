d4beda83d5f761ea9e060f70e8f9077300ad4a17
wellington.pereira <wellington.pereira@transfero.com>
2025-10-25 22:52:31 -0300

    fix createdBy

--- book-social/src/main/java/br/com/book/social/book/BookSpecification.java
+++ book-social/src/main/java/br/com/book/social/book/BookSpecification.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.domain.Specification;
 public class BookSpecification {
 
     public static Specification<Book> withOwnerId(String ownerId) {
-        return (root, query, criteriaBuilder) -> criteriaBuilder.equal(root.get("createBy"), ownerId);
+        return (root, query, criteriaBuilder) -> criteriaBuilder.equal(root.get("createdBy"), ownerId);
     }
 }
--- book-social/src/main/java/br/com/book/social/config/BeansConfig.java
+++ book-social/src/main/java/br/com/book/social/config/BeansConfig.java
@@ -41,7 +41,7 @@ public class BeansConfig {
      */
 
     @Bean
-    public AuditorAware<Integer> auditorAware() {
+    public AuditorAware<String> auditorAware() {
         return new ApplicationAuditAware();
     }
 
